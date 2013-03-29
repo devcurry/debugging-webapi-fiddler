3dbf0cd8a0c6fe2bb94a6480aa38c87214ff9220
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2013-03-29 11:49:02 +0530

    Code Commit

---
+++ WebApiWithFiddler/WebApiWithFiddler/Controllers/ValuesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApiWithFiddler.Models;
+
+namespace WebApiWithFiddler.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        List<Blog> BlogContext;
+
+        public ValuesController()
+        {
+            if (HttpContext.Current.Application["blogContext"] == null)
+            {
+                HttpContext.Current.Application["blogContext"] = new List<Blog>();
+            }
+            BlogContext = (List<Blog>)(HttpContext.Current.Application["blogContext"]);
+        }
+
+        // GET api/values
+        public IEnumerable<Blog> Get()
+        {
+            return BlogContext;
+        }
+
+        // GET api/values/5
+        public Blog Get(int id)
+        {
+            Blog current = BlogContext.Find(b => b.Id == id && !b.IsDeleted);
+            if (current == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                return current;
+            }
+        }
+
+        // POST api/values
+        public void Post([FromBody]Blog value)
+        {
+            value.Id = BlogContext.Count + 1;
+            BlogContext.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]Blog value)
+        {
+            Blog current = BlogContext.Find(b => b.Id == id);
+            if (current == null || current.IsDeleted)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                current.Title = value.Title;
+                current.Post = value.Post;
+                current.IsDeleted = value.IsDeleted;
+            }
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Blog current = BlogContext.Find(b => b.Id == id);
+            if (current == null || current.IsDeleted)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                current.IsDeleted = true;
+            }
+        }
+    }
+}
---
+++ WebApiWithFiddler/WebApiWithFiddler/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiWithFiddler.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Post { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
